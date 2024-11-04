c5f2f069c69d3c70e4cca9f98a0505d82708b160
fatima200000 <fatimazahrae.madrouni@ump.ac.ma>
2024-11-04 17:22:18 +0100

    IndexController

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/index', name: 'app_index')]
+    public function index(): Response
+    {
+        return new Response(content: "hello World!");
+    }
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return new Response(content: "hello World123!");
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return new Response(content: "willkommen!");
+    }
+}
