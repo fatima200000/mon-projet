9a2f2ddd1aa9ea7594b114aa77f9f810c3b5e552
fatima200000 <fatimazahrae.madrouni@ump.ac.ma>
2024-11-09 11:17:01 +0100

    Routing and url generation

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,15 +13,15 @@ class IndexController extends AbstractController
     {
         return new Response(content: "hello World!");
     }
-    #[Route('/about', name: 'app_about')]
-    public function about(): Response
+    #[Route('/about/{nom}', name: 'app_about')]
+    public function about( string $nom= "Anonyme"): Response
     {
-        return new Response(content: "hello World123!");
+        return new Response(content: "hallo alles 123!" .$nom);
     }
-    #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
+    #[Route('/contact/{entier}', name: 'app_contact',requirements:['entier' =>'\d+'])]
+    public function contact(string $entier = "1999"): Response
     {
-        return new Response(content: "willkommen!");
+        return new Response(content: "Willkommen!" .$entier);
     }
     #[Route('/savoirplus', name: 'app_savoirplus')]
     public function savoirplus(): Response
